0440735c31c3e948998f4cdbf961ec1b5e2a1701
Willie <w.klaas@gmx.de>
2022-10-03 11:31:29 +0200

    Update to Skypelight plus with DHT22 and DS3231 RTC

--- Arduino/SkypeStatus/SkypeStatus.ino
+++ Arduino/SkypeStatus/SkypeStatus.ino
@@ -1,5 +1,14 @@
+#include <RTClib.h>
+
+#include <DHT.h>
+#include <DHT_U.h>
+
 #include "Adafruit_NeoPixel.h"
 #include <TM1637Display.h>
+
+// DHT Sensor Pin
+#define DHTPIN 4
+#define DHTTYPE DHT22
 
 // Anzahl der LEDs
 #define NUM_LEDS 7
@@ -9,31 +18,62 @@
 #define CLK 3
 #define DIO 2
 
+// Initialisierung der RTC
+RTC_DS3231 rtc;
+char daysOfTheWeek[7][3] = { "So", "Mo", "Di", "Mi", "Do", "Fr", "Sa" };
+
 // Initialisieren der Jewel Struktur
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 TM1637Display display(CLK, DIO);
 
+// initialisierung DHT
+DHT dht(DHTPIN, DHTTYPE);
+
 // values for the display
 byte displayValues[4];
-byte eselValues[4] = {249, 109, 249, 56};
-byte easyValues[4] = {249, 247, 109, 2+4+32+64};
+byte eselValues[4] = { 249, 109, 249, 56 };
+byte easyValues[4] = { 249, 247, 109, 2 + 4 + 32 + 64 };
+byte nullValues[4] = { 0, 0, 0, 0 };
+byte deg[1] = { SEG_A + SEG_B + SEG_G + SEG_F };
+byte hum[1] = { SEG_B + SEG_C + SEG_E + SEG_F + SEG_G };
+
+byte displayBrightness = 15;
 
 void setup() {
   // Initialisieren der seriellen Verbindung zum Host
-  Serial.begin(9600);
+  Serial.begin(115200);
   // NeoPixel Bibliothek starten
   pixels.begin();
   // Willkommensmeldung ausgeben
-  Serial.println("SkypeStatus V0.1");
+  Serial.println("SkypeStatus V0.2");
 
-  display.setBrightness(0x0f);
+  display.setBrightness(displayBrightness);
   for (byte i = 0; i < 4; i++) {
     displayValues[i] = 0;
   }
   display.setSegments(displayValues);
+
+  dht.begin();
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 }
 
 long time = 0;
+long lc = 0;
 boolean blink = false;
 boolean showColon = false;
 boolean connect = false;
@@ -71,23 +111,54 @@ void loop() {
     if (time > 60) {
       time = 0;
       connect = false;
+      Serial.println("no connection");
     }
     delay(1000);
   }
 }
 
+void showDHT() {
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("h: "));
+  Serial.print(h);
+  Serial.print(F("%,t: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+}
+
+// RGB LEDs
 // verarbeiten der Daten vom Host, Format "#index,r,g,b['b']"
 // index: 0 für alle LEDs, 1..MAX_LEDS für eine einzelne LED
 // r: integer Wert der Farbe Rot 0..255
 // g: integer Wert der Farbe Grün 0..255
 // b: integer Wert der Farbe Blau 0..255
 // b: optionales Zeichen 'b' für blinkend
-//d15,249,109,249,56* means ESEL
+// Segmentanzeige:
+// d15,249,109,249,56* means ESEL
 // format für Display d(b),d1,d2,d3,d4*
 // wobei b die Helligkeit von 0..7 ist, D1, D2, D3, D4 sind die einzelnen Stellen
 // D1,3,4 haben 7 segmente, D2 hat zus. als 8'tes den Doppelpunkt
+// Temperatur
+// ein einzelnes t zeigt die aktuelle Temperatur an
+// Helligkeit der 7-Seg Anzeige
+// ein b mit folgender Nummer setzt die Helligkeit der Anzeige, 0..15
+// Luftfeuchte
+// h zeigt die aktuelle Luftfeuchtigkeit
+// Zeit
+// z setzt die aktuelle Uhrzeit auf einen neuen Wert
+// Info
+// ? sendet die aktuellen Informationen
 
-void  processPCData() {
+void processPCData() {
   // erstes Zeichen muss ein '#' sein
   char myChar = Serial.read();
   if (myChar == 'd') {
@@ -109,9 +180,9 @@ void  processPCData() {
     }
     if (myChar == '*') {
       Serial.print("time: b");
-      byte brightness = constrain(iBrightness, 0, 15);
-      display.setBrightness(brightness);
-      Serial.print(brightness);
+      displayBrightness = constrain(iBrightness, 0, 15);
+      display.setBrightness(displayBrightness);
+      Serial.print(displayBrightness);
 
       Serial.print(", seg:");
       byte value = constrain(c1, 0, 255);
@@ -136,6 +207,18 @@ void  processPCData() {
       time = 0;
       connect = true;
     }
+  } else if (myChar == 't') {
+    showTemp();
+    delay(2000);
+  } else if (myChar == 'b') {
+    int iBrightness = Serial.parseInt();
+    displayBrightness = iBrightness;
+    Serial.print("set display brightness to ");
+    Serial.println(displayBrightness);
+  } else if (myChar == 'h') {
+    Serial.println("show humidity");
+    showHumidity();
+    delay(3000);
   } else if (myChar == '#') {
     // Jetzt zunächst den Index und die 3 Farben lesen
     // Danach das b oder das Ende-Kennzeichen
@@ -174,8 +257,26 @@ void  processPCData() {
     } else {
       clear();
     }
+  } else if (myChar == 'z') {
+    // Setting the date time to the clock
+    int y = Serial.parseInt();
+    int mo = Serial.parseInt();
+    int d = Serial.parseInt();
+    int h = Serial.parseInt();
+    int m = Serial.parseInt();
+    int s = Serial.parseInt();
+    rtc.adjust(DateTime(y, mo, d, h, m, s));
+    Serial.println("time set");
+    showRTC();
+  } else if (myChar == '?') {
+    showDHT();
+    showRTC();
   }
 }
+
+long timeZ = 0;
+bool bShowTime = true;
+long actTime = 0;
 
 // Alle Farben durchgehen, aber bitte nicht zu hell...
 void fadeAllColors() {
@@ -203,11 +304,53 @@ void fadeAllColors() {
   }
   blink = false;
   showColor(0, pixels.Color(red, green, blue));
-
-  display.setBrightness(0x04);
-  display.setColon(false);
-  display.setSegments(easyValues);
+  actTime = millis();
+  if (bShowTime) {
+    if ((timeZ + 10000) > actTime) {
+      display.setColon(((actTime / 1000) % 2) == 0);
+      showTime();
+    } else {
+      bShowTime = false;
+      timeZ = actTime;
+    }
+  } else {
+    if ((timeZ + 2000) > actTime) {
+      showTemp();
+    } else {
+      bShowTime = true;
+      timeZ = actTime;
+    }
+  }
   delay(100);
+}
+
+long lastT = 0;
+int z = 0;
+
+void showTemp() {
+  if ((lastT + 1000) < millis()) {
+    float t = dht.readTemperature();
+    z = t * 10;
+    lastT = millis();
+  }
+  display.setBrightness(displayBrightness);
+  display.setColon(true);
+  display.showNumberDec(z, false, 3, 0);
+  display.setSegments(deg, 1, 3);
+}
+
+int h = 0;
+long lasth = -1;
+
+void showHumidity() {
+  float t = dht.readHumidity();
+  h = t;
+  display.clear();
+  display.setBrightness(displayBrightness);
+  display.setColon(false);
+  display.showNumberDec(h, false, 2, 0);
+  display.setSegments(hum, 1, 3);
+  lasth = h;
 }
 
 // eine LED auf Farbe setzen
@@ -237,4 +380,33 @@ void clear() {
     setLED(i, pixels.Color(0, 0, 0));
   }
   pixels.show();
+}
+// RTC functions
+void showRTC() {
+  Serial.print("z: ");
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+}
+
+void showTime() {
+  DateTime now = rtc.now();
+  byte h = now.hour();
+  byte m = now.minute();
+  display.setBrightness(displayBrightness);
+  display.showNumberDec(m, true, 2, 2);
+  display.showNumberDec(h, false, 2, 0);
 }
--- README.md
+++ README.md
@@ -6,3 +6,61 @@ The jewel is coverd with a translucent cube.
 The data exchange is very simple, using the USB-COM port driver of the arduino nano. 
 See more information on my google+ account.
 https://plus.google.com/+WilfriedKlaas/posts/TpiskEiGzVi
+
+The plus version now implements an DHT22 Sensor for temp and humidity and a DS3231 RTC for the time.
+
+For the RGB LED Jewel (7 RGB LEDs) you need the adafruit NeoPixel lib (https://github.com/adafruit/Adafruit_NeoPixel)
+
+For the DHT you need the DHT Sensor Lib (https://github.com/adafruit/DHT-sensor-library)
+
+For the RTC you'll need the adafruit RTCLib (https://github.com/adafruit/RTClib)
+
+All this can be installed via Library manager.
+
+For the TM1637 Display you need my fork of the TM1637Display lib. (https://github.com/willie68/TM1637) This have to be installed manually.
+
+## Controll via Serial Interface
+
+You can controll the skyplight via a serial control interface.
+
+The following commands are known:
+
+### RGB LEDs
+
+to controll the RGB LEDs you must send a command with the Format "#index,r,g,b['b']" with
+
+- index: 0 for all LEDs, 1..MAX_LEDS for a single LED
+- r: integer value of red 0..255
+- g: integer value of green 0..255
+- b: integer value of blue 0..255
+- b: optional char 'b' for blink
+
+### 7-Segment display
+
+sending the command format for the display "d(b),d1,d2,d3,d4*"
+
+example d15,249,109,249,56* means ESEL
+
+- b brighttness from 0..15
+- D1, D2, D3, D4 are the single segments
+-  D1,3,4 have 7 segments, D2 has the colon as an 8's 
+
+### Brightness of the 7-Seg Display
+
+b sets the brightness of the default display, 0..15
+
+### Temperatur
+
+a single T show the actual temperature (from the DHT 22 Sensor)
+
+### Humidity
+
+h show the actual humidity 
+
+### Time
+
+z sets the actual time of the RTC, format z yyyy,mm,dd,hh,mm,ss
+
+### Info
+
+With an ? you can read internal sensor data from the RTC and DHT
